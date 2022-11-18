17f20669ed1dd7e01f0e1cfe33289032c161333b
Alistair White <alistair@pik-potsdam.de>
2022-11-18 17:29:49 +0100

    Add relu as default activation

--- src/neural_nets.jl
+++ src/neural_nets.jl
@@ -1,4 +1,4 @@
-function get_mlp((input_size, output_size)::Pair, hidden_layers, hidden_width, activation)
+function get_mlp((input_size, output_size)::Pair, hidden_layers, hidden_width, activation = relu)
     mlp = Chain(
         Dense(input_size => hidden_width, activation),                                 # Input layer
         [Dense(hidden_width => hidden_width, activation) for _ = 1:hidden_layers]...,  # Hidden layers
