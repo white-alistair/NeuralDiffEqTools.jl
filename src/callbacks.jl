78077d7b612ad74fbcadcd84626aa09e5357290a
Alistair White <alistair@pik-potsdam.de>
2022-11-25 18:42:40 +0100

    Use CairoMakie for progress plots

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -1,15 +1,23 @@
-function cb_display(epoch, steps, iter, tspan, loss, target, pred, t; verbose, show_plot)
+function cb_display(epoch, steps, iter, tspan, loss, target, pred, times; verbose, show_plot)
     if verbose
         @info @sprintf "[epoch = %04i] [steps = %02i] [iter = %04i] [tspan = (%05.2f, %05.2f)] Loss = %.2e\n" epoch steps iter tspan[1] tspan[2] loss
     end
-    # if show_plot
-    #     plot_prediction(system, target, pred, t)  # Method defined in the same file as the given system
-    # end
+    if show_plot
+        plot_prediction(target, pred, times)
+    end
     return false
 end
 
-function plot_prediction(target, pred, t, layout, title)
-    plt = Plots.plot(t, transpose(target), layout = layout, title = title, label = "target")
-    Plots.plot!(plt, t, transpose(pred), layout = layout, label = "predicted")
-    display(plt)
+function plot_prediction(ground_truth, predicted, times)
+    fig = CairoMakie.Figure()
+    dim = size(ground_truth)[1]
+    for i in 1:dim
+        ax = Axis(fig[i, 1])
+        lines_ground_truth = lines!(ax, times, ground_truth[i, :])
+        lines_predicted = lines!(ax, times, predicted[i, :])
+        if i == dim
+            Legend(fig[1:dim, 2], [lines_ground_truth, lines_predicted], ["ground truth", "predicted"])
+        end
+    end
+    display(fig)
 end
