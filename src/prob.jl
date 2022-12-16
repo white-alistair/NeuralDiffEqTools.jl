ed46d6d018a878ac2673f82fd0adc388e071ace4
Alistair White <alistair@pik-potsdam.de>
2022-12-16 18:40:31 +0100

    Add in_place option to get_prob

--- src/prob.jl
+++ src/prob.jl
@@ -1,3 +1,3 @@
-function get_prob(rhs!, θ::Vector{T}, N) where {T}
-    return ODEProblem{true,SciMLBase.FullSpecialize}(rhs!, rand(T, N), (T(0), T(1)), θ)
+function get_prob(rhs, θ::Vector{T}, N; in_place = true, specialize = SciMLBase.FullSpecialize) where {T}
+    return ODEProblem{in_place,specialize}(rhs, rand(T, N), (T(0), T(1)), θ)
 end
