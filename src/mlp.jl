44a390d3be7a46f26337b492e4a244030973c4e0
Alistair White <alistair@pik-potsdam.de>
2023-01-12 11:40:39 +0100

    Correct number of hidden layers for MLP

--- src/mlp.jl
+++ src/mlp.jl
@@ -1,8 +1,13 @@
-function get_mlp((input_size, output_size)::Pair, hidden_layers, hidden_width, activation = relu)
+function get_mlp(
+    (input_size, output_size)::Pair,
+    hidden_layers,
+    hidden_width,
+    activation = relu,
+)
     mlp = Chain(
-        Dense(input_size => hidden_width, activation),                                 # Input layer
-        [Dense(hidden_width => hidden_width, activation) for _ = 1:hidden_layers]...,  # Hidden layers
-        Dense(hidden_width => output_size),                                            # Output layer
+        Dense(input_size => hidden_width, activation),                                     # First hidden layer
+        [Dense(hidden_width => hidden_width, activation) for _ = 1:(hidden_layers-1)]...,  # Remaining hidden layers
+        Dense(hidden_width => output_size),                                                # Output layer
     )
     θ, re = Flux.destructure(mlp)
     return θ, re
