ad1f701168c44f5f34d139631760462838c221f1
Alistair White <alistair@pik-potsdam.de>
2023-01-15 20:39:40 +0100

    Refactor learning curve plot

--- src/learning_curves.jl
+++ src/learning_curves.jl
@@ -18,7 +18,7 @@ function plot_learning_curve(model_id::Integer)
 end
 
 function plot_learning_curve(filepath::String)
-    data, header = readdlm(filepath, ',', header = true)
+    data, header = readdlm(filepath, ','; header = true)
     epoch, steps, learning_rate, training_loss, validation_loss, duration = eachcol(data)
     return plot_learning_curve(
         epoch,
@@ -38,37 +38,40 @@ function plot_learning_curve(
     validation_loss,
     duration,
 )
-    f = CairoMakie.Figure(resolution = (1000, 1000))
-
-    step_epochs = [epoch[findfirst(step .== steps)] for step in unique(steps)]
-    step_labels = string.(Int.(unique(steps)))
+    f = CairoMakie.Figure(; resolution = (1000, 1000))
 
     # 1. Plot training and validation loss
     ax1 = Axis(
-        f[1:3, 1],
+        f[1:4, 1];
         ylabel = "validation loss",
         yscale = log10,
+        yticks = LogTicks(IntegerTicks()),
         yaxisposition = :right,
-        xticks = step_epochs,
         yminorticksvisible = true,
         yminorgridvisible = true,
         yminorticks = IntervalsBetween(8),
     )
 
-    ax2 = Axis(f[1:3, 1], ylabel = "training loss", yscale = log10, ygridvisible = false)
+    ax2 = Axis(
+        f[1:3, 1];
+        ylabel = "training loss",
+        yscale = log10,
+        yticks = LogTicks(IntegerTicks()),
+        ygridvisible = false,
+    )
     hidespines!(ax2)
     hidexdecorations!(ax2)
 
-    train_plot = lines!(ax2, epoch, training_loss, color = :blue)
-    valid_plot = lines!(ax1, epoch, validation_loss, color = :red)
+    train_plot = lines!(ax2, epoch, training_loss; color = :blue)
+    valid_plot = lines!(ax1, epoch, validation_loss; color = :red)
 
     min_val_loss, min_val_epoch = findmin(validation_loss)
-    min_val_plot = hlines!(ax1, min_val_loss, linestyle = :dash)
-    vlines!(ax1, min_val_epoch, linestyle = :dash)
+    min_val_plot = hlines!(ax1, min_val_loss; linestyle = :dash)
+    vlines!(ax1, min_val_epoch; linestyle = :dash)
     Legend(
-        f[1:3, 1],
+        f[1:4, 1],
         [train_plot, valid_plot, min_val_plot],
-        ["training loss", "validation loss", "minimum validation loss"],
+        ["training loss", "validation loss", "minimum validation loss"];
         margin = (10, 10, 10, 10),
         tellheight = false,
         tellwidth = false,
@@ -78,8 +81,7 @@ function plot_learning_curve(
 
     # 2. Plot learning rate
     ax3 = Axis(
-        f[4, 1],
-        xticks = step_epochs,
+        f[5, 1];
         yscale = log10,
         yticks = LogTicks(IntegerTicks()),
         ylabel = "learning rate",
@@ -87,47 +89,15 @@ function plot_learning_curve(
     linkxaxes!(ax1, ax3)
     lines!(ax3, epoch, learning_rate)
 
-    # 3. Plot epoch duration
-    ax4 = Axis(
-        f[5, 1],
-        xticks = step_epochs,
-        xlabel = "epoch",
-        ylabel = "duration",
-    )
+    # 3. Plot training steps
+    ax4 = Axis(f[6, 1]; ylabel = "steps", yticks = IntegerTicks())
     linkxaxes!(ax1, ax4)
-    lines!(ax4, epoch, duration)
+    lines!(ax4, epoch, steps)
 
-    # 4. Add labels and ticks for integration steps
-    ax5 = Axis(
-        f[1:3, 1],
-        xlabel = "integration steps",
-        xaxisposition = :top,
-        xticks = (step_epochs, step_labels),
-        xticklabelsvisible = true,
-    )
-    hidespines!(ax5)
-    hideydecorations!(ax5)
+    # 4. Plot epoch duration
+    ax5 = Axis(f[7, 1]; xlabel = "epoch", ylabel = "duration [s]")
     linkxaxes!(ax1, ax5)
-
-    ax6 = Axis(
-        f[4, 1],
-        xaxisposition = :top,
-        xticks = (step_epochs, step_labels),
-        xticklabelsvisible = true,
-    )
-    hidespines!(ax6)
-    hideydecorations!(ax6)
-    linkxaxes!(ax1, ax6)
-
-    ax7 = Axis(
-        f[5, 1],
-        xaxisposition = :top,
-        xticks = (step_epochs, step_labels),
-        xticklabelsvisible = true,
-    )
-    hidespines!(ax7)
-    hideydecorations!(ax7)
-    linkxaxes!(ax1, ax7)
+    lines!(ax5, epoch, duration)
 
     return f
 end
