254cb702f97c4f729d046cf77b70ec5087b25f7b
Alistair White <alistair@pik-potsdam.de>
2023-01-19 18:19:54 +0100

    Add tests for normalised error calc

--- test/valid_time.jl
+++ test/valid_time.jl
@@ -1,19 +1,44 @@
 @testset "valid_time.jl" begin
-    normalised_error = [0.3, 0.3, 0.5, 0.6]
-    times = [0.1, 0.2, 0.3, 0.4]
-    valid_time =
-        NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
-    @test valid_time ≈ 0.2
+    @testset "Normalised error calc" begin
+        ground_truth = [1.0 -1.0 1.0]
+        predicted = [0.8 1.2 0.7]
+        E = NeuralDiffEqTools.get_normalised_error(ground_truth, predicted)
+        @test E ≈ [0.2, 2.2, 0.3]
 
-    normalised_error = [0.0, 0.3, 0.3]
-    times = [0.0, 0.1, 0.2]
-    valid_time =
-        NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
-    @test valid_time ≈ 0.2
+        ground_truth = [1.0 1.0 1.0]
+        predicted = [1.2 0.8 1.3]
+        E = NeuralDiffEqTools.get_normalised_error(ground_truth, predicted)
+        @test E ≈ [0.2, 0.2, 0.3]
 
-    normalised_error = [0.0, 0.4, 0.3]
-    times = [0.0, 0.1, 0.2]
-    valid_time =
-        NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
-    @test valid_time ≈ 0.0
+        ground_truth = [
+            3.0 4.0 3.0
+            4.0 3.0 4.0
+        ]
+        predicted = [
+            3.0 4.0 3.0
+            4.5 4.0 4.0
+        ]
+        E = NeuralDiffEqTools.get_normalised_error(ground_truth, predicted)
+        @test E ≈ [0.1, 0.2, 0.0]
+    end
+
+    @testset "Valid Time calc" begin
+        normalised_error = [0.3, 0.3, 0.5, 0.6]
+        times = [0.1, 0.2, 0.3, 0.4]
+        valid_time =
+            NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
+        @test valid_time ≈ 0.2
+
+        normalised_error = [0.0, 0.3, 0.3]
+        times = [0.0, 0.1, 0.2]
+        valid_time =
+            NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
+        @test valid_time ≈ 0.2
+
+        normalised_error = [0.0, 0.4, 0.3]
+        times = [0.0, 0.1, 0.2]
+        valid_time =
+            NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
+        @test valid_time ≈ 0.0
+    end
 end
