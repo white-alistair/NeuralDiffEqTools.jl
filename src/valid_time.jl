a4feadbc4a5cfe93ee8b2c97f8c03ab28b00b918
Alistair White <alistair@pik-potsdam.de>
2023-01-22 16:16:28 +0100

    Rename variable

--- src/valid_time.jl
+++ src/valid_time.jl
@@ -1,20 +1,20 @@
 function get_normalised_error(
-    target::AbstractMatrix{T},
-    pred::AbstractMatrix{T},
+    ground_truth::AbstractMatrix{T},
+    predicted::AbstractMatrix{T},
 ) where {T<:AbstractFloat}
     return vec(
-        sqrt.(sum(abs2, target .- pred; dims = 1)) /
-        sqrt(mean(sum(abs2, target; dims = 1))),
+        sqrt.(sum(abs2, ground_truth .- predicted; dims = 1)) /
+        sqrt(mean(sum(abs2, ground_truth; dims = 1))),
     )
 end
 
 function get_valid_time(
-    target::AbstractMatrix{T},
-    pred::AbstractMatrix{T},
+    ground_truth::AbstractMatrix{T},
+    predicted::AbstractMatrix{T},
     times::AbstractVector{T};
     error_threshold::T,
 ) where {T<:AbstractFloat}
-    normalised_error = get_normalised_error(target, pred)
+    normalised_error = get_normalised_error(ground_truth, predicted)
     return get_valid_time(normalised_error, times; error_threshold)
 end
 
