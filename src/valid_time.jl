ec6ea1d3ec1a1ba72ce4a670c181a018d7e80eb3
Alistair White <alistair@pik-potsdam.de>
2023-01-25 10:05:14 +0100

    Fix valid error threshold kwarg

--- src/valid_time.jl
+++ src/valid_time.jl
@@ -12,22 +12,22 @@ function get_valid_time(
     ground_truth::AbstractMatrix{T},
     predicted::AbstractMatrix{T},
     times::AbstractVector{T};
-    error_threshold::T,
+    valid_error_threshold::T,
 ) where {T<:AbstractFloat}
     normalised_error = get_normalised_error(ground_truth, predicted)
-    return get_valid_time(normalised_error, times; error_threshold)
+    return get_valid_time(normalised_error, times; valid_error_threshold)
 end
 
 function get_valid_time(
     normalised_error::AbstractVector{T},
     times::AbstractVector{T};
-    error_threshold::T,
+    valid_error_threshold::T,
 ) where {T<:AbstractFloat}
-    error_threshold_index = findfirst(error -> error >= error_threshold, normalised_error)
-    if isnothing(error_threshold_index)
+    valid_error_index = findfirst(error -> error >= valid_error_threshold, normalised_error)
+    if isnothing(valid_error_index)
         valid_time = times[end]
     else
-        valid_time = times[error_threshold_index - 1]
+        valid_time = times[valid_error_index-1]
     end
     return valid_time
 end
--- test/valid_time.jl
+++ test/valid_time.jl
@@ -26,19 +26,19 @@
         normalised_error = [0.3, 0.3, 0.5, 0.6]
         times = [0.1, 0.2, 0.3, 0.4]
         valid_time =
-            NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
+            NeuralDiffEqTools.get_valid_time(normalised_error, times; valid_error_threshold = 0.4)
         @test valid_time ≈ 0.2
 
         normalised_error = [0.0, 0.3, 0.3]
         times = [0.0, 0.1, 0.2]
         valid_time =
-            NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
+            NeuralDiffEqTools.get_valid_time(normalised_error, times; valid_error_threshold = 0.4)
         @test valid_time ≈ 0.2
 
         normalised_error = [0.0, 0.4, 0.3]
         times = [0.0, 0.1, 0.2]
         valid_time =
-            NeuralDiffEqTools.get_valid_time(normalised_error, times; error_threshold = 0.4)
+            NeuralDiffEqTools.get_valid_time(normalised_error, times; valid_error_threshold = 0.4)
         @test valid_time ≈ 0.0
     end
 end
