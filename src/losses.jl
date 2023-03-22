0669d9f7d20db3277d9e2b93270d1489d56396b7
Alistair White <alistair@pik-potsdam.de>
2023-03-22 15:41:18 +0100

    Do not compute MSE on u0

--- src/losses.jl
+++ src/losses.jl
@@ -1,3 +1,3 @@
 function MSE(prediction::AbstractMatrix{T}, target::AbstractMatrix{T}) where {T<:Real}
-    return mean(abs2, prediction .- target)
+    return mean(abs2, prediction[:, 2:end] .- target[:, 2:end])  # Do not include u0
 end
