8fae6809c8a739f232652c95e8ad6cc7956d08e6
Alistair White <alistair@pik-potsdam.de>
2023-03-22 15:41:05 +0100

    Simplify multiple shooting

--- src/multiple_shooting.jl
+++ src/multiple_shooting.jl
@@ -1,28 +1,8 @@
-struct MultipleShooting{T}
-    time_series::TimeSeries{T}
-    step_size::Int
-    shuffle::Bool
-end
-
-MultipleShooting(time_series::TimeSeries{T}, step_size::Int) where {T} =
-    MultipleShooting{T}(time_series, step_size, true)
-
-Base.length(ms::MultipleShooting) = floor(Int32, length(ms.time_series) / ms.step_size)
-Base.eltype(::Type{MultipleShooting{T}}) where {T} = Tuple{Vector{T},Matrix{T}}
-
-function Base.iterate(ms::MultipleShooting)
-    N = length(ms.time_series)
-    start_indexes = collect(1:ms.step_size:N-ms.step_size)
-    if ms.shuffle
+function multiple_shooting(time_series::TimeSeries, step::Int, shuffle = true)
+    (; times, trajectory) = time_series
+    start_indexes = collect(1:step:length(time_series)-step)
+    if shuffle
         shuffle!(start_indexes)
     end
-    iterate(ms, start_indexes)
-end
-
-function Base.iterate(ms::MultipleShooting, start_indexes)
-    isempty(start_indexes) && return nothing
-    start_index = pop!(start_indexes)
-    times = @view ms.time_series.times[start_index:start_index+ms.step_size]
-    trajectory = @view ms.time_series.trajectory[:, start_index:start_index+ms.step_size]
-    return ((times, trajectory), start_indexes)
+    return [(times[i:i+step], trajectory[:, i:i+step]) for i in start_indexes]
 end
