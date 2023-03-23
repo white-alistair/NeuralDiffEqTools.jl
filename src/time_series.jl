2d7805ed57d5df2323215421e7fa78c0c59934b5
Alistair White <alistair@pik-potsdam.de>
2023-03-23 12:25:25 +0100

    Remove chunk method

--- src/time_series.jl
+++ src/time_series.jl
@@ -20,20 +20,3 @@ Base.getindex(ode_solution::TimeSeries, I::UnitRange) = ode_solution.trajectory[
 Base.firstindex(ode_solution::TimeSeries) = 1
 Base.lastindex(ode_solution::TimeSeries) = size(ode_solution.trajectory)[2]
 Base.length(ode_solution::TimeSeries) = length(ode_solution.times)
-
-function chunk(time_series::TimeSeries{T}, chunk_size::Int) where {T}
-    (; times, trajectory) = time_series
-    last_dim = ndims(trajectory)
-    N = size(trajectory)[end]
-    
-    chunks = TimeSeries{T}[]
-    for i in 1:(chunk_size-1):N
-        time_chunk = times[i:min(i + chunk_size - 1, N)]
-        traj_chunk = selectdim(trajectory, last_dim, i:min(i + chunk_size - 1, N)) 
-        if length(time_chunk) > 1
-            push!(chunks, TimeSeries{T}(time_chunk, traj_chunk))
-        end
-    end
-
-    return chunks
-end
