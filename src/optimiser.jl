04cc666d9770ccb1a73c0fdb9c4a626bf3d9f8e2
Alistair White <alistair@pik-potsdam.de>
2023-03-28 21:22:20 +0200

    Use Symbol for rule_type

--- src/optimiser.jl
+++ src/optimiser.jl
@@ -1,7 +1,7 @@
 function get_optimiser(rule_type, hyperparameters)
-    if rule_type == "Adam"
+    if rule_type == :Adam
         rule = Optimisers.Adam()
-    elseif rule_type == "AdamW"
+    elseif rule_type == :AdamW
         rule = Optimisers.AdamW()
     end
     return Optimisers.adjust(rule; hyperparameters...)
