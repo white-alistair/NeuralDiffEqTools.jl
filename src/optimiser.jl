df6704a1a31623604c03c3d64255a79535671605
Alistair White <alistair@pik-potsdam.de>
2023-03-28 20:25:41 +0200

    Add get_optimiser method

---
+++ src/optimiser.jl
@@ -0,0 +1,8 @@
+function get_optimiser(rule_type, hyperparameters)
+    if rule_type == "Adam"
+        rule = Optimisers.Adam()
+    elseif rule_type == "AdamW"
+        rule = Optimisers.AdamW()
+    end
+    return Optimisers.adjust(rule; hyperparameters...)
+end
