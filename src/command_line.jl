5dbc095ab4a0d1b13e23c33016d6718a25a9f0ec
Alistair White <alistair@pik-potsdam.de>
2023-04-11 23:30:29 +0200

    Make manual GC a flag

--- src/command_line.jl
+++ src/command_line.jl
@@ -60,9 +60,8 @@ function get_common_settings()
         "--time-limit", "--time"
             arg_type = Float32
             default = Inf32
-        "--n-manual-gc"
-            arg_type = Int
-            default = 0
+        "--manual-gc"
+            action = :store_true
 
         # Solver args
         "--reltol"
--- src/train.jl
+++ src/train.jl
@@ -16,7 +16,7 @@ function train!(
     time_limit = 23 * 60 * 60.0f0,
     verbose = false,
     show_plot = false,
-    n_manual_gc = 0,
+    manual_gc = false,
 ) where {T<:AbstractFloat}
     @info "Beginning training..."
 
@@ -104,8 +104,8 @@ function train!(
             break
         end
 
-        # Manually call the GC a few times to (hopefully) avoid OOM errors
-        for _ in 1:n_manual_gc
+        # Manually call the GC to (hopefully) avoid OOM errors
+        if manual_gc
             GC.gc(true)
             ccall(:malloc_trim, Cvoid, (Cint,), 0)
         end
