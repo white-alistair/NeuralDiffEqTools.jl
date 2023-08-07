ec302c8403df391dc1ca26b89e9c324ce2035ad8
Alistair White <alistair@pik-potsdam.de>
2023-08-07 11:33:17 +0200

    Update multiple shooting function signature

--- src/multiple_shooting.jl
+++ src/multiple_shooting.jl
@@ -1,8 +1,8 @@
-function multiple_shooting(time_series::TimeSeries, step::Int, shuffle = false)
+function multiple_shooting(time_series::TimeSeries; steps::Int, shuffle = false)
     (; times, trajectory) = time_series
-    start_indexes = collect(1:step:length(time_series)-step)
+    start_indexes = collect(1:steps:length(time_series)-steps)
     if shuffle
         shuffle!(start_indexes)
     end
-    return [(times[i:i+step], trajectory[:, i:i+step]) for i in start_indexes]
+    return [(times[i:i+steps], trajectory[:, i:i+steps]) for i in start_indexes]
 end
