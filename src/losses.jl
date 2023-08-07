07c9746fc676e9f66c8f82047a8dce2dd480eb22
Alistair White <alistair@pik-potsdam.de>
2023-08-07 11:33:45 +0200

    Remove type annotations from MSE

--- src/losses.jl
+++ src/losses.jl
@@ -1,6 +1,6 @@
 function MSE(
-    predicted_trajectory::AbstractMatrix{T},
-    target_trajectory::AbstractMatrix{T},
-) where {T}
+    predicted_trajectory,
+    target_trajectory,
+)
     return mean(abs2, predicted_trajectory[:, 2:end] .- target_trajectory[:, 2:end])  # Do not include u0
 end
